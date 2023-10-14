355065cbc0fdc734808feb4eaf0b5b42af83ba0e
Bruno Miguel Sousa Da Costa <bsousa-d@c1r11s8.42porto.com>
2023-10-14 16:46:28 +0100

    added the bonus functions

--- src/ft_lstadd_back.c
+++ src/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bsousa-d <bsousa-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:14:47 by bsousa-d          #+#    #+#             */
-/*   Updated: 2023/10/09 15:16:37 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2023/10/09 16:04:58 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*node;
 
 	node = *lst;
-	if (!lst)
-		return ;
 	if (!*lst)
 	{
 		*lst = new;
